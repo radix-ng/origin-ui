675beb0b422a0c543227dc59689013ae42b3a671
Oleg Pimenov <pimenov.o@gmail.com>
2024-12-06 12:14:11 +0300

    chore: check defer

--- apps/origin-ui/src/app/pages/buttons/page.component.ts
+++ apps/origin-ui/src/app/pages/buttons/page.component.ts
@@ -17,12 +17,16 @@ import { AppPageHeaderComponent } from '../../components/page-header.component';
                     <div
                         class="[&>*]:before:bg-border/70 [&>*]:after:bg-border/70 grid max-w-6xl grid-cols-1 overflow-hidden sm:grid-cols-2 lg:grid-cols-3 [&>*]:relative [&>*]:px-1 [&>*]:py-12 [&>*]:before:absolute [&>*]:before:[block-size:100vh] [&>*]:before:[inline-size:1px] [&>*]:before:[inset-block-start:0] [&>*]:before:[inset-inline-start:-1px] [&>*]:after:absolute [&>*]:after:[block-size:1px] [&>*]:after:[inline-size:100vw] [&>*]:after:[inset-block-start:-1px] [&>*]:after:[inset-inline-start:0] sm:[&>*]:px-8 xl:[&>*]:px-12"
                     >
-                        @for (item of files; track item) {
-                            <app-demo-component
-                                [componentName]="item"
-                                directory="buttons"
-                                className="text-center"
-                            ></app-demo-component>
+                        @defer {
+                            @for (item of files; track item) {
+                                <app-demo-component
+                                    [componentName]="item"
+                                    directory="buttons"
+                                    className="text-center"
+                                ></app-demo-component>
+                            }
+                        } @placeholder {
+                            <div class="text-sm flex justify-center">Loading...</div>
                         }
                     </div>
                 </div>
