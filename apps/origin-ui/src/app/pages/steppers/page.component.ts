0a32cfa67c3ded1fa31dde937ef06ee71b4b1516
Oleg Pimenov <pimenov.o@gmail.com>
2025-02-21 14:38:07 +0300

    feat(stepper): added vertical Stepper examples

--- apps/origin-ui/src/app/pages/steppers/page.component.ts
+++ apps/origin-ui/src/app/pages/steppers/page.component.ts
@@ -14,14 +14,30 @@ import { AppPageHeaderComponent } from '../../components/page-header.component';
                         TailwindCSS.
                     </app-components-page-header>
 
-                    <div
-                        class="[&>*]:before:bg-border/70 [&>*]:after:bg-border/70 grid max-w-6xl grid-cols-1 overflow-hidden [&>*]:relative [&>*]:px-1 [&>*]:py-12 [&>*]:before:absolute [&>*]:before:[block-size:100vh] [&>*]:before:[inline-size:1px] [&>*]:before:[inset-block-start:0] [&>*]:before:[inset-inline-start:-1px] [&>*]:after:absolute [&>*]:after:[block-size:1px] [&>*]:after:[inline-size:100vw] [&>*]:after:[inset-block-start:-1px] [&>*]:after:[inset-inline-start:0] sm:[&>*]:px-8 xl:[&>*]:px-12"
-                    >
-                        @defer {
-                            @for (componentName of files; track componentName) {
-                                <app-demo-component [componentName]="componentName" directory="steppers" />
+                    <div class="overflow-hidden">
+                        <div
+                            class="*:not-first:-ms-px *:not-first:-mt-px -m-px grid grid-cols-12 *:px-1 *:py-12 sm:*:px-8 xl:*:px-12"
+                        >
+                            @defer {
+                                @for (componentName of files; track componentName) {
+                                    <app-demo-component
+                                        class="col-span-12 border-[0.5px] has-[[data-comp-loading=true]]:border-none sm:col-span-12 lg:col-span-12"
+                                        [componentName]="componentName"
+                                        directory="steppers"
+                                    />
+                                }
                             }
-                        }
+
+                            @defer {
+                                @for (componentName of filesVertical; track componentName) {
+                                    <app-demo-component
+                                        class="col-span-12 border-[0.5px] has-[[data-comp-loading=true]]:border-none sm:col-span-6 lg:col-span-6"
+                                        [componentName]="componentName"
+                                        directory="steppers"
+                                    />
+                                }
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
@@ -32,5 +48,10 @@ export default class PageSlidersComponent {
     files = [
         'stepper-01',
         'stepper-05'
+    ];
+
+    filesVertical = [
+        'stepper-14',
+        'stepper-15'
     ];
 }
