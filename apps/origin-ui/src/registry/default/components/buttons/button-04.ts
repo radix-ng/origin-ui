99a91cd2161dab43df814fac864e623795d74dee
Oleg Pimenov <pimenov.o@gmail.com>
2025-03-02 09:48:33 +0300

    chore: fixed reference to tailwindcss

--- apps/origin-ui/src/registry/default/components/buttons/button-04.ts
+++ apps/origin-ui/src/registry/default/components/buttons/button-04.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { OriButton } from '~/registry/default/ui/button';
 import { Archive, LucideAngularModule } from 'lucide-angular';
+import { OriButton } from '~/registry/default/ui/button';
 
 @Component({
     selector: 'demo-button-04',
@@ -12,6 +12,7 @@ import { Archive, LucideAngularModule } from 'lucide-angular';
         </button>
     `,
     styles: `
+        @reference "tailwindcss";
         ::ng-deep .icon-wrapper svg {
             @apply -ms-1 me-2 opacity-60;
         }
